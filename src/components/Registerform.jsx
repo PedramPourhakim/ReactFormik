f694cddc1668ea93e4645b793f7f2724fe72a3c9
Pedram <shemuel1226@gmail.com>
2023-03-29 20:16:54 +0430

    until session 71

--- src/components/Registerform.jsx
+++ src/components/Registerform.jsx
@@ -1,52 +1,141 @@
-import React from 'react';
-import { useFormik } from 'formik'
+import React from "react";
+import {
+  ErrorMessage,
+  FastField,
+  Field,
+  Form,
+  Formik,
+  useFormik,
+} from "formik";
+import * as Yup from "yup";
+import PersonalField from "./PersonalField";
+import PersonalError from "./PersonalError";
 
+const initialValues = {
+  name: "Pedram",
+  email: "",
+  password: "",
+  bio: "",
+};
+const onSubmit = () => {};
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "لطفا این قسمت را پر کنید";
+  }
+  if (!values.email) {
+    errors.email = "لطفا این قسمت را پر کنید";
+  } else if (
+    !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)
+  ) {
+    errors.email = "لطفا قالب ایمیل را رعایت کنید مثال : shemuel1226@gmail.com";
+  }
+  if (!values.password) {
+    errors.password = "لطفا این قسمت را پر کنید";
+  }
+  return errors;
+};
+const validationSchema = Yup.object({
+  name: Yup.string().required("لطفا این قسمت را پر کنید"),
+  email: Yup.string()
+    .required("لطفا این قسمت را پر کنید")
+    .email("لطفا قالب ایمیل را رعایت کنید مثال : shemuel1226@gmail.com"),
+  password: Yup.string()
+    .required("لطفا این قسمت را پر کنید")
+    .min(8, "حداقل 8 کاراکتر وارد کنید"),
+});
 const Registerform = () => {
-    const formik = useFormik({
-        initialValues:{
-            name: 'qasem',
-            email: '',
-            password: '',
-        },
-        onSubmit: values=>{
-            console.log(values);
-        }
-    })
+  // const formik = useFormik({
+  //  initialValues,
+  //   onSubmit,
+  //   // validate,
+  //   validationSchema
+  // });
+  const attrs = {
+    type: "text",
+    className: "form-control",
+    id: "name",
+    name: "name",
+  };
 
-    return (
-        <div className='auth_container container-fluid d-flex justify-content-center align-items-center w-100 h-100-vh p-0'>
-            <div className="row w-100 justify-content-center align-items-center">
-                <div className='auth_box col-11 col-md-8 col-lg-6 col-xl-4 py-4 px-3'>
-                    <form onSubmit={formik.handleSubmit}>
-                        <h1 className='text-center'>
-                            <i className='fas fa-user-plus text-primary'></i>
-                        </h1>
-                        <div className="mb-3">
-                            <label htmlFor="name" className="form-label">نام</label>
-                            <input type="text" className="form-control" id="name" name='name'
-                            value={formik.values.name} onChange={formik.handleChange}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="email" className="form-label">ایمیل</label>
-                            <input type="email" className="form-control" id="email" name='email'
-                            value={formik.values.email} onChange={formik.handleChange}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="password" className="form-label">رمز عبور</label>
-                            <input type="password" className="form-control" id="password" name='password'
-                            value={formik.values.password} onChange={formik.handleChange}
-                            />
-                        </div>
-                        <div className='text-center w-100'>
-                            <button type="submit" className="btn btn-primary">ثبت نام</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <div className="auth_container container-fluid d-flex justify-content-center align-items-center w-100 h-100-vh p-0">
+        <div className="row w-100 justify-content-center align-items-center">
+          <div className="auth_box col-11 col-md-8 col-lg-6 col-xl-4 py-4 px-3">
+            <Form>
+              <h1 className="text-center">
+                <i className="fas fa-user-plus text-primary"></i>
+              </h1>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  نام
+                </label>
+                <FastField
+                  {...attrs}
+                  placeholder="لطفا از حروف لاتین استفاده کنید"
+                />
+                <ErrorMessage name="name" component={PersonalError} />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  ایمیل
+                </label>
+                <FastField
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  // {...formik.getFieldProps("email")}
+                />
+                <ErrorMessage name="email">
+                  {error => (
+                    <small
+                      className="d-block text-center 
+                  text-danger">
+                      {error}
+                    </small>
+                  )}
+                </ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  رمز عبور
+                </label>
+                <FastField name="password">
+                  {(props) => <PersonalField {...props} />}
+                </FastField>
+                {/* <ErrorMessage name="password" /> */}
+              </div>
+              <div className="mb-3">
+                <label htmlFor="bio" className="form-label">
+                  بیوگرافی
+                </label>
+                <FastField
+                  type="text"
+                  className="form-control"
+                  id="bio"
+                  name="bio"
+                  component="textarea"
+                  // {...formik.getFieldProps("password")}
+                />
+                <ErrorMessage name="bio" />
+              </div>
+              <div className="text-center w-100">
+                <button type="submit" className="btn btn-primary">
+                  ثبت نام
+                </button>
+              </div>
+            </Form>
+          </div>
         </div>
-    );
-}
+      </div>
+    </Formik>
+  );
+};
 
 export default Registerform;
